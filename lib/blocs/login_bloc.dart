f22194399757c246a0c3250df7896d74404c1a23
hrxiang <xianghairui@126.com>
2021-09-23 19:13:53 +0800

    fix bug

--- lib/blocs/login_bloc.dart
+++ lib/blocs/login_bloc.dart
@@ -18,6 +18,8 @@ class LoginBloc extends BlocBase {
   final textCtrl = TextEditingController();
   final uuid = Uuid();
   late String account;
+  final idList = ["18349115126", "13918588195", "17396220460", "18666662412"];
+  final gid = 'b6d1f6d16481547c26b744fa701c6f0f';
 
   LoginBloc() {
     account = SpUtil.getString("account");
@@ -46,8 +48,9 @@ class LoginBloc extends BlocBase {
           'uid': textCtrl.text,
           'name': 'Ox${textCtrl.text}',
         });
-        //////////////
-       /* resp = await dio.post<Map<String, dynamic>>(Config.IP_LOGIN, data: {
+        ///////////////////////////////////忽略以下逻辑/////////////////////////////////
+        // 管理员登录
+      /*  resp = await dio.post<Map<String, dynamic>>(Config.IP_LOGIN, data: {
           'secret': Config.secret,
           'platform': plat,
           'uid': 'openIM123456',
@@ -55,30 +58,33 @@ class LoginBloc extends BlocBase {
         Map? result = resp.data!['data'];
         uid = result!['uid'];
         token = result['token'];
-        // 导入
+        // 为用户导入好友
         resp = await dio.post<Map<String, dynamic>>(
             'http://1.14.194.38:10000/friend/import_friend',
             data: {
-              "uid": "21979710c3fe454d",
-              "operationID": "1111111222",
-              "ownerUid": "f732156059eeb5d0"
+              "uidList": idList,
+              "ownerUid": textCtrl.text,
+              "operationID": "1111111111111",
             },
             options: Options(headers: {
               'token': token,
             }));
-        // 进组
+        print('----------------导入好友成功');
+        // 把用户拉进组
         resp = await dio.post<Map<String, dynamic>>(
             'http://1.14.194.38:10000/group/invite_user_to_group',
             data: {
-              "groupID": "f21f9f84c14f3a978352ff339f1a800a",
-              "uidList": ["openIM123456", ""],
-              "reason": "reason",
+              "groupID": gid,
+              "uidList": idList,
+              "reason": "default op",
               "operationID": "1111111111111 "
             },
             options: Options(headers: {
               'token': token,
-            }));*/
-        ///////////////////////
+            }));
+        print('----------------进组成功');*/
+        ////////////////////////////////////////////////////////////////////////////
+
         isRegistered = true;
       } catch (e) {
         //{"errCode":500,"errMsg":"rpc error: code = Unknown desc = Error 1062: Duplicate entry '333333' for key 'PRIMARY'"}
@@ -103,7 +109,7 @@ class LoginBloc extends BlocBase {
         // print('register data:$data');
         resp = await dio.post<Map<String, dynamic>>(Config.IP_LOGIN, data: {
           'secret': Config.secret,
-          'platform': Platform.isAndroid ? IMPlatform.android : IMPlatform.ios,
+          'platform': plat,
           'uid': textCtrl.text,
         });
         Map? result = resp.data!['data'];
@@ -114,10 +120,9 @@ class LoginBloc extends BlocBase {
 
       if (uid.isNotEmpty && token.isNotEmpty) {
         SpUtil.putString('account', textCtrl.text);
-        SpUtil.putString(textCtrl.text, uid);
         OpenIM.iMManager
             .login(uid: uid, token: token)
-            .then((value) => SpUtil.putString("account", textCtrl.text))
+            // .then((value) => SpUtil.putString("account", textCtrl.text))
             .then((value) => NavigatorManager.startMain())
             .catchError((e) => _showError(e))
         // .catchError((e) => OpenIM.iMManager.logout())
@@ -125,13 +130,13 @@ class LoginBloc extends BlocBase {
       }
     } catch (e) {
       print('e:$e');
-      // var error = e as DioError;
+      var error = e as DioError;
       // print('e--:${error.response}');
       // print('e--:${error.response.runtimeType}');
       // print('e--:${error.response?.data}');
       // print('e--:${error.response?.data.runtimeType}');
       // print('e--:${error.response?.data['errMsg']}');
-      _showError(e);
+      _showError(error.response?.data['errMsg'] ?? e);
     }
   }
 
